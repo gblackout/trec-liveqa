f3b7961a4d886babc4f836b306c5aa839611a9b4
Yuan Yang <yuanyang@Yuans-MacBook-Air.local>
2017-06-27 16:42:26 -0400

    bug fix

--- cnn-text-classification-tf/text_cnn.py
+++ cnn-text-classification-tf/text_cnn.py
@@ -4,88 +4,6 @@ import sys
 import itertools
 
 
-class TextCNN(object):
-    """
-    A CNN for text classification.
-    Uses an embedding layer, followed by a convolutional, max-pooling and softmax layer.
-    """
-
-    def __init__(self, sequence_length, num_classes, vocab_size, embedding_size, filter_sizes, num_filters,
-                 multilabel=0, pooling_filter_size=3, l2_reg_lambda=0.0, weighted_loss=0):
-
-        # Placeholders for input, output and dropout
-        self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
-        self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
-        if weighted_loss:
-            self.sample_weight = tf.placeholder(tf.float32, [None, 1], name="sample_weight")
-        self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
-        l2_loss = tf.constant(0.0)
-
-        # TODO Embedding layer
-        # with tf.device('/cpu:0'), tf.name_scope("embedding"):
-        with tf.name_scope("embedding"):
-            W = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0), name="W")
-            self.embedded_chars = tf.nn.embedding_lookup(W, self.input_x)
-            self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
-
-        # Create a convolution + maxpool layer for each filter size
-        pooled_outputs = []
-        for i, filter_size in enumerate(filter_sizes):
-            with tf.name_scope("conv-maxpool-%s" % filter_size):
-                # Convolution Layer
-                filter_shape = [filter_size, embedding_size, 1, num_filters]
-                W = tf.Variable(tf.truncated_normal(filter_shape, stddev=0.1), name="W")
-                b = tf.Variable(tf.constant(0.1, shape=[num_filters]), name="b")
-                conv = tf.nn.conv2d(self.embedded_chars_expanded, W, strides=[1, 1, 1, 1], padding="VALID", name="conv")
-
-                # Apply nonlinearity
-                h = tf.nn.relu(tf.nn.bias_add(conv, b), name="relu")
-
-                # Maxpooling over the outputs
-                pooled = tf.nn.max_pool(h, ksize=[1, sequence_length - filter_size + 1, 1, 1], strides=[1, 1, 1, 1],
-                                        padding='VALID', name="pool")
-                pooled_outputs.append(pooled)
-                print('filter_size : {}, pooled : {}'.format(filter_size, pooled.get_shape()))
-
-        # Combine all the pooled features
-        num_filters_total = num_filters * len(filter_sizes)
-        self.h_pool = tf.concat(pooled_outputs, 3)
-        self.h_pool_flat = tf.reshape(self.h_pool, [-1, num_filters_total])
-        print('h_pool_flat : {}'.format(self.h_pool_flat.get_shape()))
-
-        # Add dropout
-        with tf.name_scope("dropout"):
-            self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
-
-        # Final (unnormalized) scores and predictions
-        with tf.name_scope("output"):
-            W = tf.get_variable("W", shape=[num_filters_total, num_classes],
-                                initializer=tf.contrib.layers.xavier_initializer())
-            b = tf.Variable(tf.constant(0.1, shape=[num_classes]), name="b")
-            l2_loss += tf.nn.l2_loss(W)
-            l2_loss += tf.nn.l2_loss(b)
-            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="scores")
-            self.scores = tf.nn.batch_normalization(self.scores, 0, 1, None, None, 0.01, name='bn')
-            self.predictions = tf.argmax(self.scores, 1, name="predictions")
-
-        # Mean cross-entropy loss
-        with tf.name_scope("loss"):
-            if multilabel:
-                losses = tf.nn.sigmoid_cross_entropy_with_logits(logits=self.scores, labels=self.input_y)
-            else:
-                if not weighted_loss:
-                    losses = tf.losses.sparse_softmax_cross_entropy(logits=self.scores, labels=self.input_y)
-                else:
-                    losses = tf.losses.sparse_softmax_cross_entropy(logits=self.scores, labels=self.input_y,
-                                                                    weights=self.sample_weight, scope=None)
-            self.loss = tf.reduce_mean(losses) + l2_reg_lambda * l2_loss
-
-        # Accuracy
-        with tf.name_scope("accuracy"):
-            correct_predictions = tf.equal(self.predictions, tf.argmax(self.input_y, 1))
-            self.accuracy = tf.reduce_mean(tf.cast(correct_predictions, "float"), name="accuracy")
-
-
 class TextCNN_V2(object):
     def __init__(self, sequence_length, num_classes, vocab_size, embedding_size, filter_sizes, num_filters, dense_size,
                  l2_coef, crf_lambda_doub, crf_lambda_cub, crf_lambda_quad, use_crf=True, init_w2v=None,
@@ -115,13 +33,13 @@ class TextCNN_V2(object):
         """
         # Placeholders for input, output and dropout
         self.input_x = tf.placeholder(tf.int32, [None, sequence_length], name="input_x")
-        self.input_wiki = tf.placeholder(tf.float32, [None, sequence_length], name="input_wiki")
+        self.input_wiki = tf.placeholder(tf.int32, [None, sequence_length], name="input_wiki")
         self.input_adm = tf.placeholder(tf.float32, [None, num_classes], name="input_adm")
         self.input_y = tf.placeholder(tf.float32, [None, num_classes], name="input_y")
 
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
         self.is_training = tf.placeholder(tf.bool, name='is_training')
-        self.population = tf.placeholder(tf.int32, shape=num_classes, name="input_x")
+        self.population = tf.placeholder(tf.int32, shape=num_classes, name="population")
 
         # all possible tags: permute * num_classes
         self.all_y = np.array([seq for seq in itertools.product([0, 1], repeat=num_classes)], dtype=np.float32)
@@ -140,7 +58,7 @@ class TextCNN_V2(object):
         self.l2_loss = None
 
         with tf.name_scope("concat_wiki"):
-            self.input_x = tf.concat(self.input_wiki, self.input_x, axis=0) # (num_class+batch_size) X seq_len
+            self.input_wiki_x = tf.concat([self.input_wiki, self.input_x], axis=0) # (num_class+batch_size) X seq_len
 
         # either learn a new one or load from a pretrained-one
         with tf.name_scope("embedding"):
@@ -155,7 +73,7 @@ class TextCNN_V2(object):
                 W = tf.Variable(tf.random_uniform([vocab_size, embedding_size], -1.0, 1.0), name="W")
                 print '======>init random word2vec'
 
-            self.embedded_chars = tf.nn.embedding_lookup(W, self.input_x)
+            self.embedded_chars = tf.nn.embedding_lookup(W, self.input_wiki_x)
             self.embedded_chars_expanded = tf.expand_dims(self.embedded_chars, -1)
 
         # Create a convolution + maxpool layer for each channel
@@ -194,31 +112,32 @@ class TextCNN_V2(object):
         with tf.name_scope("dropout"):
             self.h_drop = tf.nn.dropout(self.h_pool_flat, self.dropout_keep_prob)
 
-        # split wiki; concat adm; tile wiki; concate wiki by class
+        # split wiki; concat adm; tile wiki; concat wiki by class
         with tf.name_scope('split_concat_tile'):
-            self.wiki_features = self.h_drop[:num_classes]
-            self.h_drop = self.h_drop[num_classes:]
+            wiki_features = self.h_drop[:num_classes]
+            ehr_features = self.h_drop[num_classes:]
+            numOf_batch = tf.shape(ehr_features)[0]
 
-            self.h_drop = tf.concat([self.h_drop, self.input_adm], axis=-1)
+            ehr_adm_features = tf.concat([ehr_features, self.input_adm], axis=-1, name='concat_adm')
 
             feature_by_class = []
-            numOf_batch = tf.shape(self.scores)[0]
             for i in xrange(num_classes):
-                wiki_mat = tf.tile(self.wiki_features[i], [numOf_batch])
-                feature_by_class.append(tf.concat([self.scores, wiki_mat], axis=-1))
+                wiki_mat = tf.tile(wiki_features[i], [numOf_batch])
+                wiki_mat = tf.reshape(wiki_mat, [numOf_batch, num_filters_total])
+                feature_by_class.append(tf.concat([ehr_adm_features, wiki_mat], axis=-1, name='concat_wiki_%i' % i))
 
-            # (num_class X batch_size) X (num_filters_total+num_class+num_class)
-            self.h_drop = tf.concat(feature_by_class, axis=0)
+            # (num_class X batch_size) X (num_filters_total+num_class+num_filters_total)
+            self.all_features = tf.concat(feature_by_class, axis=0)
 
         # relu dense layer
         with tf.name_scope("dense"):
             # TODO ad hoc
-            W = tf.Variable(tf.truncated_normal(shape=[num_filters_total + num_classes * 2, dense_size], stddev=0.1),
+            W = tf.Variable(tf.truncated_normal(shape=[num_filters_total*2 + num_classes, dense_size], stddev=0.1),
                             name="W")
             self.l2_loss += tf.nn.l2_loss(W)
             b = tf.Variable(tf.constant(0.1, shape=[dense_size]), name="b")
 
-            self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name="lin_transform")
+            self.scores = tf.nn.xw_plus_b(self.all_features, W, b, name="lin_transform")
             self.scores = tf.contrib.layers.batch_norm(self.scores, is_training=self.is_training)
             self.scores = tf.nn.relu(self.scores, name="relu")
 
@@ -309,6 +228,7 @@ class TextCNN_V2(object):
         with tf.name_scope("accuracy"):
             if use_crf:
                 inds = tf.argmax(all_loglikelihood, axis=-1, name='inds')
+                inds = tf.cast(inds, tf.int32)
                 iter_ind = tf.constant(0)
                 preds = tf.constant([[-1.0] * num_classes])
 
